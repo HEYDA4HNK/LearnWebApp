e2f0fc1c836f6f5f709051f9c0dc96dca23c03dc
HEYDA4HNK <salabanovsasa@gmail.com>
2026-09-14 04:19:57 +0500

    Добавлена авторизация и аутентификация пользователя

--- LearnWebApp/Program.cs
+++ LearnWebApp/Program.cs
@@ -1,10 +1,59 @@
-var builder = WebApplication.CreateBuilder(args);
 
+using Core;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Persistence;
+using LearnWebApp.Configs;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddHttpLogging();
+
+#region UserServicesBind
+builder.Services
+    .AddScoped<UserManager<AppUser>>();
+builder.Services.AddDbContext<IdentityDbContext<AppUser>, AppUserDbContext>(
+    options =>
+    {
+        options.UseNpgsql(configuration.GetConnectionString(nameof(AppUserDbContext)));
+    }
+);
+builder.Services
+    .AddIdentity<AppUser, IdentityRole>(options =>
+    {
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireDigit = false;
+        options.User.RequireUniqueEmail = true;
+    })
+    .AddEntityFrameworkStores<AppUserDbContext>();
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidIssuer = JwtBearerConfig.Issuer,
+            ValidateAudience = true,
+            ValidAudience = JwtBearerConfig.Audience,
+            ValidateLifetime = true,
+            IssuerSigningKey = JwtBearerConfig.GetKey(),
+            ValidateIssuerSigningKey = true,
+        };
+    });
+
+builder.Services
+    .AddAuthorization();
+#endregion
 
 var app = builder.Build();
 
@@ -13,11 +62,9 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+app.UseHttpLogging();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.MapControllers();
-
 app.Run();
